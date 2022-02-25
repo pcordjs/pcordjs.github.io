20ccabc17f148642df842649a2d37ed9048a60f8
doinkythederp <doinkythederp@icloud.com>
2022-02-24 21:04:23 -0800

    feat(Header): use icon-only links on small screens

--- src/HeaderLink.tsx
+++ src/HeaderLink.tsx
@@ -12,8 +12,8 @@ export default function HeaderLink(props: HeaderLinkProps) {
       href={props.href}
       class="text-xl align-middle h-3/4 inline-flex items-center px-3 py-2.5 mr-2 last:mr-0 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg"
     >
-      <i class={`${props.icon}`}></i>
-      <span class="ml-2 font-light">{props.children}</span>
+      <i class={`${props.icon} text-2xl sm:text-xl`}></i>
+      <span class="ml-2 font-light hidden sm:inline">{props.children}</span>
     </a>
   );
 }
