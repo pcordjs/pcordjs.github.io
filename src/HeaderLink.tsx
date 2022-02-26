1805a88ca22bb0d0577003de930d0d34ea464407
doinkythederp <doinkythederp@icloud.com>
2022-02-25 20:40:09 -0800

    feat(Header): underline links on hover

--- src/HeaderLink.tsx
+++ src/HeaderLink.tsx
@@ -10,10 +10,12 @@ export default function HeaderLink(props: HeaderLinkProps) {
   return (
     <a
       href={props.href}
-      class="text-xl align-middle h-3/4 inline-flex items-center px-3 py-2.5 mr-2 last:mr-0 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg"
+      class="text-xl align-middle h-3/4 inline-flex items-center px-3 py-2.5 mr-2 last:mr-0 hover:bg-gray-100 dark:hover:bg-gray-800 rounded-lg group"
     >
       <i class={`${props.icon} text-2xl sm:text-xl`}></i>
-      <span class="ml-2 font-light hidden sm:inline">{props.children}</span>
+      <span class="ml-2 font-light hidden sm:inline group-hover:underline">
+        {props.children}
+      </span>
     </a>
   );
 }
