9a0f3d8daf4f7ef32232906dffc9a5f1204e1ad8
doinkythederp <doinkythederp@icloud.com>
2022-02-25 20:43:45 -0800

    feat(PageContent): add margin to content

--- src/PageContent.tsx
+++ src/PageContent.tsx
@@ -6,8 +6,10 @@ export interface PageContentProps {
 
 export default function PageContent(props: PageContentProps) {
   return (
-    <main class="m-auto pt-14 max-w-prose text-lg text-center space-y-4">
-      {props.children}
-    </main>
+    <div class="mx-4 sm:mx-8">
+      <main class="m-auto pt-14 max-w-prose text-lg text-center space-y-4">
+        {props.children}
+      </main>
+    </div>
   );
 }
